fdbace9011d8d1b0fc1b42f0048aa936c2e9199a
Alex Ford <fordas@uw.edu>
2018-06-09 10:18:38 -0700

    get/post logging handlers

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  console.log(req);
+
   res.send('Hello World!');
+});
+
+app.post(function(req, res){
+  console.log(req);
+
+  res.status(400);
+  res.send();
 });
 
 app.listen(3000, function () {
