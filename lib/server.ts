034936927516f8130d2d164f10cc2d030f475005
Alex Ford <fordas@uw.edu>
2018-06-09 14:16:03 -0700

    Covert example to typescript app.

--- lib/server.ts
+++ lib/server.ts
@@ -1,19 +1,50 @@
-var express = require('express');
-var app = express();
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
 
-app.get('/', function (req, res) {
-  console.log(req);
+class App {
 
-  res.send('Hello World!');
-});
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-app.post('/github-webhook', function(req, res){
-  console.log(req);
+  public app: express.Application;
 
-  res.status(202);
-  res.send();
-});
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  private routes(): void {
+    const router = express.Router();
+
+    router.get('/', (req: Request, res: Response) => {
+
+      console.log(req.url);
+      console.log(req.body);
+
+      res.status(200).send({
+        message: 'Hello World!'
+      })
+    });
+
+    router.post('/github-webhook', (req: Request, res: Response) => {
+      console.log(req.url);
+      console.log(req.body);
+
+      res.status(202).send();
+    });
+
+    this.app.use('/', router)
+  }
+}
+
+let main = new App();
+
+const port = 3000;
+
+main.app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
 });
