a9331fe06b4d955032beeb73ed596e88eae2ca6e
Alex Ford <fordas@uw.edu>
2018-06-10 17:37:43 -0700

    Parse hook body via co-body.

--- src/webhooks/github.ts
+++ src/webhooks/github.ts
@@ -1,47 +1,44 @@
 import * as express from "express";
-import * as bodyParser from "body-parser"; 
+import * as co_body from "co-body";
 import { Request, Response } from "express";
 
 import * as sign from '@octokit/webhooks/sign'
-const secret = 'mysecret'; 
+const secret = 'mysecret';
 
-function verify_signature(req: Request, res: Response, buf: Buffer, encoding) {
-  const sig = req.headers['x-hub-signature']
-  const local_sig = sign(secret, buf.toString())
-  console.log("x-hub-sig: ", sig)
-  console.log("payload-sig: ", sign(secret, buf))
+async function handler(req: Request, res: Response, next) {
+  try {
 
-  if(sig !== local_sig){
-    console.log("Invalid signature.")
+    // Get body (and raw body for validation), unpack content type
+    let { parsed: body, raw: raw_body } = await co_body(req, {returnRawBody: true})
 
-    res.status(400)
-      .send({"error": "Invalid x-hub-signature."})
+    if (req.header["content-type"] == "application/x-www-form-urlencoded") {
+      body = body["payload"]
+    }
 
-    throw "Invalid signature."
+    // Validate request body via signature
+    const sig = req.headers['x-hub-signature']
+    const local_sig = sign(secret, raw_body)
+    console.log("x-hub-sig: ", sig)
+    console.log("payload-sig: ", local_sig)
+
+    if(sig !== local_sig){
+      console.log("Invalid signature.")
+
+      res.status(400).send({"error": "Invalid x-hub-signature."})
+      return
+    }
+
+    const name = req.headers['x-github-event']
+    const id = req.headers['x-github-delivery']
+
+    console.log("name: ", name)
+    console.log("id: ", id)
+    console.log(body)
+
+    res.status(200).send()
+  } catch(e) {
+    next(e)
   }
 }
-
-function process_hook(req: Request, res: Response) {
-  const name = req.headers['x-github-event']
-  const id = req.headers['x-github-delivery']
-
-  console.log("name: ", name)
-  console.log("id: ", id)
-  console.log(req)
-
-  // Extract payload from body if needed.
-
-  res.status(200).send()
-}
-
-//handler.on('*', ({id, name, payload}) => {
-//  console.log("event: ", name);
-//})
-//
-const handler = [
-  bodyParser.json({verify: verify_signature}),
-  bodyParser.urlencoded({extended: true, verify: verify_signature}),
-  process_hook,
-]
 
 export { handler }
