6ca24536f7c2721bf2324b4f533f55f76df088eb
Alex Ford <fordas@uw.edu>
2018-06-10 19:45:42 -0700

    Add github app config and github-app dependencies.

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,31 @@
 import * as debug from "debug";
 import * as express from "express";
+import * as create_github_app from "github-app";
+import * as fs from "fs";
 import { Request, Response } from "express";
 import { set_zen, handler as zen_handler} from './zen';
 import { handler as github_handler, emitter as github_events } from './webhooks/github';
 
 const logger = debug("gh-app-sand:server")
 
-let app = express()
+const github_app_config = {
+  id: fs.readFileSync("secrets/github-app.id").toString().trim(),
+  cert: fs.readFileSync('secrets/github-app.private-key.pem')
+}
+const github_app = create_github_app(github_app_config)
+logger("github_app.id: ", github_app_config.id)
+
+let service = express()
 const port = 3000
 
-app.post("/webhooks/github", github_handler)
+service.post("/webhooks/github", github_handler)
 github_events.on("ping", function(body) {
   logger("received zen:", body.zen)
   set_zen(body.zen)
 })
 
-app.get('/zen', zen_handler)
+service.get('/zen', zen_handler)
 
-app.listen(port, function() {
+service.listen(port, function() {
   console.log('Express server listening on port ' + port)
 })
